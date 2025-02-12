4521a4da4a2c273f3a2fc88dbd9c6f042fbd0728
Relentless <dardankabashi101@gmail.com>
2025-02-12 12:17:21 +0100

    Update appwrite.js

--- src/appwrite.js
+++ src/appwrite.js
@@ -10,7 +10,7 @@ const client = new Client()
 
 const database = new Databases(client);
 
-export const updateSearchCount = async (searchTerm, movie) => {
+export const updateSearchCount = async (searchTerm, movie) => {  
   // 1. Use Appwrite SDK to check if the search term exists in the database
   try {
     const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
