9002156eab3bf84159a5bb0d78779ee29d241bf8
Dardan Kabashi <dardankabashi101@gmail.com>
2026-04-20 23:41:34 +0200

    Update Appwrite configuration and endpoint

--- src/appwrite.js
+++ src/appwrite.js
@@ -1,12 +1,12 @@
 import { Client, Databases, ID, Query } from "appwrite";
 
-const PROJECT_ID = import.meta.env.VITE_APPWRITE_PROJECT_ID; 
-const DATABASE_ID = import.meta.env.VITE_APPWRITE_DATABASE_ID;
-const COLLECTION_ID = import.meta.env.VITE_APPWRITE_COLLECTION_ID; 
+const PROJECT_ID = "69e696110018c6ea4061";
+const DATABASE_ID = "69e69676001ff7816562";
+const COLLECTION_ID = "metrics";
 
 const client = new Client()
-  .setEndpoint("https://cloud.appwrite.io/v1")  
-  .setProject(PROJECT_ID);  
+  .setEndpoint("https://fra.cloud.appwrite.io/v1")
+  .setProject(PROJECT_ID);
 
 const database = new Databases(client);
 
